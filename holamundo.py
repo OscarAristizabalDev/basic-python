52bf88bd7074d3fb28a83e0331b621a3f8b328f5
oscartolima20 <oscartolima@hotmail.com>
2021-01-24 21:49:19 +0000

    Primeros avances en python

---
+++ holamundo.py
@@ -0,0 +1,14 @@
+# imports
+import sys
+
+# print('Hola mundo!')
+# print(sys.version)
+
+# Print string as error message
+
+#frint("Hello, Python!")
+#print("Hello, Python!)
+      
+print("This will be printed")
+frint("This will cause an error")
+print("This will NOT be printed")
